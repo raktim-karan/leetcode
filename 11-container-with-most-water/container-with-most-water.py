cdede3f3ec3cf18c89965dbd21b16ba3d798cfb9
Raktim Karan <118724193+raktim-karan@users.noreply.github.com>
2026-09-03 13:59:06 +0530

    Time: 119 ms (55.12%) | Memory: 20.6 MB (64.61%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,17 +1,25 @@
 class Solution(object):
     def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
         left = 0
         right = len(height) - 1
-        max_area = 0
-
+        max_water = 0
+        
         while left < right:
+            # Calculate the width and the limiting height
             width = right - left
-            h = min(height[left], height[right])
-            max_area = max(max_area, width * h)
-
+            current_height = min(height[left], height[right])
+            
+            # Update max_water if the current container holds more
+            max_water = max(max_water, width * current_height)
+            
+            # Greedily move the pointer pointing to the shorter line
             if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
-
-        return max_area
+                
+        return max_water
